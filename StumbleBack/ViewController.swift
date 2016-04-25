abada6268c526253ba23964917d1230069f403dd
Jeff Chimney <jeffchimney@AVSs-Mac-mini.local>
2016-04-25 13:39:43 -0700

    Added map and placeholder nav bar at bottom

---
+++ StumbleBack/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StumbleBack
+//
+//  Created by Jeff Chimney on 2016-04-25.
+//  Copyright © 2016 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
