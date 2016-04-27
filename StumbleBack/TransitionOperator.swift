bb9c7a9cb6f422b798f558d0e82ab605bbaf9c95
Jeff Chimney <jeffchimney@AVSs-Mac-mini.local>
2016-04-27 16:19:14 -0700

    Finished show and dismiss side menu.  Still need to hook up menu items to their views and make the side menu look nice.

--- StumbleBack/TransitionOperator.swift
+++ StumbleBack/TransitionOperator.swift
@@ -12,12 +12,21 @@ import UIKit
 class TransitionOperator: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate{
     
     var snapshot : UIView!
+    var isPresenting : Bool = true
     
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-        return 0.5
+        return 0.75
     }
     
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        if isPresenting{
+            presentNavigation(transitionContext)
+        }else{
+            dismissNavigation(transitionContext)
+        }
+    }
+    
+    func presentNavigation(transitionContext: UIViewControllerContextTransitioning) {
         let container = transitionContext.containerView()
         let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
         let fromView = fromViewController!.view
@@ -25,7 +34,7 @@ class TransitionOperator: NSObject, UIViewControllerAnimatedTransitioning, UIVie
         let toView = toViewController!.view
         
         let size = toView.frame.size
-        var offSetTransform = CGAffineTransformMakeTranslation(size.width - 120, 0)
+        var offSetTransform = CGAffineTransformMakeTranslation(size.width - size.width/2, 0)
         offSetTransform = CGAffineTransformScale(offSetTransform, 0.6, 0.6)
         
         snapshot = fromView.snapshotViewAfterScreenUpdates(true)
@@ -45,11 +54,32 @@ class TransitionOperator: NSObject, UIViewControllerAnimatedTransitioning, UIVie
         })
     }
     
+    func dismissNavigation(transitionContext: UIViewControllerContextTransitioning) {
+        
+        let container = transitionContext.containerView()
+        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
+        let fromView = fromViewController!.view
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
+        let toView = toViewController!.view
+        
+        let duration = self.transitionDuration(transitionContext)
+        
+        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: {
+            
+            self.snapshot.transform = CGAffineTransformIdentity
+            
+            }, completion: { finished in
+                transitionContext.completeTransition(true)
+                self.snapshot.removeFromSuperview()
+        })
+    }
+    
     func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.isPresenting = true
         return self
     }
     
     func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return self
-    }
+        self.isPresenting = false
+        return self    }
 }
